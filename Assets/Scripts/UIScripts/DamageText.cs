b39d4758ff04ca08ef97321656c661edca7e5aaf
DESKTOP-1JTU406\PC - 01 <44286137+sparserman@users.noreply.github.com>
2022-11-08 15:29:42 +0900

    v1.0.9

--- Assets/Scripts/UIScripts/DamageText.cs
+++ Assets/Scripts/UIScripts/DamageText.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
+//using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class DamageText : MonoBehaviour
 {
